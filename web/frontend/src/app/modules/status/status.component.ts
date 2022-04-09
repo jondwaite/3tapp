1eb7f85c0eaf76381ca9f0da191f613cd11fd070
jondwaite <jonwaite1@gmail.com>
2022-04-09 13:04:49 +0000

    status page changes

--- web/frontend/src/app/modules/status/status.component.ts
+++ web/frontend/src/app/modules/status/status.component.ts
@@ -6,6 +6,7 @@ import { environment } from 'src/environments/environment';
 export interface StatusData {
   item: string; // Configuration key from Status API return
   value: string; // Current value of the key
+  desired: boolean; // Boolean representation of whether in desired state
   status: boolean; // Boolean representation of the status
   class: string; // which display class should be assigned ('ok','warn','none')
 }
@@ -29,7 +30,7 @@ export class StatusComponent implements OnInit {
   dbStatus: StatusData[];
   appStatus: StatusData[];
   webStatus: StatusData[];
-  columnsToDisplay = ['item', 'value', 'status'];
+  columnsToDisplay = ['item', 'value'];
 
   constructor(private apiService: ApiService) {}
 
@@ -55,36 +56,42 @@ export class StatusComponent implements OnInit {
           item: 'Database Service',
           value: this.dbData['db_running'] ? 'Running' : 'Stopped',
           status: this.dbData['db_running'],
+          desired: true,
           class: this.dbData['db_running'] ? 'ok' : 'warn'
         },
         {
           item: 'TCP/3306',
           value: this.dbData['tcp_3306_open'] ? 'Open' : 'No Response',
           status: this.dbData['tcp_3306_open'],
+          desired: true,
           class: this.dbData['tcp_3306_open'] ? 'ok' : 'warn'
         },
         {
           item: 'Web Server Connectivity',
           value: this.dbData['web_ping'],
           status: this.dbData['web_ping'] ? true : false,
+          desired: false,
           class: this.dbData['web_ping'] ? 'warn' : 'ok'
         },
         {
           item: 'Web port 80 Access',
           value: this.dbData['web_80_open'] ? 'Open' : 'No Response',
           status: this.dbData['web_80_open'],
+          desired: false,
           class: this.dbData['web_80_open'] ? 'warn' : 'ok'
         },
         {
           item: 'App Server Connectivity',
           value: this.dbData['app_ping'],
           status: this.dbData['app_ping'] ? true : false,
+          desired: false,
           class: this.dbData['app_ping'] ? 'warn': 'ok'
         },
         {
           item: 'App port 3002 Access',
           value: this.dbData['app_3002_open'] ? 'Open' : 'No Response',
           status: this.dbData['app_3002_open'],
+          desired: false,
           class: this.dbData['app_3002_open'] ? 'warn' : 'ok'
         },
       ];
@@ -98,36 +105,42 @@ export class StatusComponent implements OnInit {
           item: 'API Service',
           value: this.appData['3tapp_app_running'] ? 'Running' : 'Stopped',
           status: this.appData['3tapp_app_running'],
+          desired: true,
           class: this.appData['3tapp_app_running'] ? 'ok' : 'warn'
         },
         {
           item: 'TCP/3002',
           value: this.appData['app_3002_open'] ? 'Open' : 'No Response',
           status: this.appData['app_3002_open'],
+          desired: true,
           class: this.appData['app_3002_open'] ? 'ok' : 'warn'
         },
         {
           item: 'Web Server Connectivity',
           value: this.appData['web_ping'],
           status: this.appData['web_ping'] ? true : false,
+          desired: false,
           class: this.appData['web_ping'] ? 'warn' : 'ok'
         },
         {
           item: 'Web port 80 Access',
           value: this.appData['web_80_open'] ? 'Open' : 'No Response',
           status: this.appData['web_80_open'],
+          desired: false,
           class: this.appData['web_80_open'] ? 'warn' : 'ok'
         },
         {
           item: 'DB Server Connectivity',
           value: this.appData['db_ping'],
           status: this.appData['db_ping'] ? true : false,
+          desired: true,
           class: this.appData['db_ping'] ? 'ok' : 'warn'
         },
         {
           item: 'DB port 3306 Access',
           value: this.appData['db_3306_open'] ? 'Open' : 'No Response',
           status: this.appData['db_3306_open'],
+          desired: true,
           class: this.appData['db_3306_open'] ? 'ok' : 'warn'
         },
       ];
@@ -137,46 +150,53 @@ export class StatusComponent implements OnInit {
       this.webData = x['status'];
       // console.log('webData:' + JSON.stringify(this.webData));
       this.webStatus = [
-        {
-          item: 'Nginx Service',
-          value: this.webData['nginx_running'] ? 'Running' : 'Stopped',
-          status: this.webData['nginx_running'],
-          class: this.webData['nginx_running'] ? 'ok' : 'warn'
-        },
+        // {
+        //   item: 'Nginx Service',
+        //   value: this.webData['nginx_running'] ? 'Running' : 'Stopped',
+        //   status: this.webData['nginx_running'],
+        //   desired: true,
+        //   class: this.webData['nginx_running'] ? 'ok' : 'warn'
+        // },
         {
           item: 'Web Application',
           value: this.webData['3tapp_web_running'] ? 'Running' : 'Stopped',
           status: this.webData['3tapp_web_running'],
+          desired: true,
           class: this.webData['3tapp_web_running'] ? 'ok' : 'warn'
         },
         {
           item: 'TCP/80 Open',
           value: this.webData['web_80_open'] ? 'Open' : 'No Response',
           status: this.webData['web_80_open'],
+          desired: true,
           class: this.webData['web_80_open'] ? 'ok' : 'warn'
         },
         {
           item: 'App Server Connectivity',
           value: this.webData['app_ping'],
           status: this.webData['app_ping'] ? true : false,
+          desired: true,
           class: this.webData['app_ping'] ? 'ok' : 'warn'
         },
         {
           item: 'App port 3002 Access',
           value: this.webData['app_3002_open'] ? 'Open' : 'No Response',
           status: this.webData['app_3002_open'],
+          desired: true,
           class: this.webData['app_3002_open'] ? 'ok' : 'warn'
         },
         {
           item: 'DB Server Connectivity',
           value: this.webData['db_ping'],
           status: this.webData['db_ping'] ? true : false,
+          desired: false,
           class: this.webData['db_ping'] ? 'warn' : 'ok'
         },
         {
           item: 'DB port 3306 Access',
           value: this.webData['db_3306_open'] ? 'Open' : 'No Response',
           status: this.webData['db_3306_open'],
+          desired: false,
           class: this.webData['db_3306_open'] ? 'warn' : 'ok'
         },
       ];
